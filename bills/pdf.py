8aa83140021baa53e6e64f3eaf2cdfcac044e3f0
Shail Murtaza <58264560+ShailMurtaza@users.noreply.github.com>
2022-03-28 18:52:37 +0500

    Add files via upload

---
+++ bills/pdf.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkdocviewer import *
+
+# Create a root window
+root = Tk()
+
+# Create a DocViewer widget
+v = DocViewer(root)
+v.pack(side="top", expand=1, fill="both")
+
+# Display some document
+v.display_file("bill.pdf")
+
+# Start Tk's event loop
+root.mainloop()
